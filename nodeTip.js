c283280fdd0f27c4b4379ec6416be3aba3d7b226
minhoshin <minho.shin@gmail.com>
2018-04-27 14:54:32 +0900

    callback ..

--- nodeTip.js
+++ nodeTip.js
@@ -207,3 +207,17 @@ console.log('----------------------------');
 studentList.forEach( student => {
     console.log(`${student.name} total: ${plusRe(student.math, student.science)}`);
 });
+
+// callback -------------------------------------
+
+const callbackSum = (a, b) => a + b;
+
+const printResult = (result) => {
+    console.log(`결과는 ${result} 입니다.`);
+};
+
+const calculationAndPrint =  (calculationResult, callback) => {
+  callback(calculationResult);
+};
+
+calculationAndPrint(callbackSum(10, 20), printResult);
